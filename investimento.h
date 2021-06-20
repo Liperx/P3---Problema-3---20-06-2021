0a90ccc3fbd887a9e8d6cb9416cdb487bf3782a0
liperx <82781588+Liperx@users.noreply.github.com>
2021-06-20 13:41:54 +0000

    Adicionado o comando de struct no arquivo .h e alteração no main da struct

--- investimento.h
+++ investimento.h
@@ -4,7 +4,28 @@
 
 using namespace std;
 
-float Simples(float principal, float taxa, float anos)
+struct tInvestimentos{
+  float cinValorInicial(){
+    float vInicial;
+    cout << "Digite o capital inicial: R$"; 
+    cin >> vInicial;
+    return (vInicial);
+  }
+  float cinTaxa(){
+    float taxa;
+    cout << "Digite a taxa anual: "; 
+    cin >> taxa;
+    return (taxa);
+  }
+  float cinTempo(){
+    float tempo;
+    cout << "Digite o tempo de aplicacao em anos: "; 
+    cin >> tempo;
+    return (tempo);
+  }
+};
+
+void Simples(float principal, float taxa, float anos)
 { 
   taxa = taxa / 100; //transformando pra usar nas contas
   float juros = principal * taxa * anos;
@@ -13,8 +34,7 @@ float Simples(float principal, float taxa, float anos)
   cout << "O total de juros a ser pago é: " << juros << "\n";
   cout << "O montante a ser pago é: " << montante << "\n\n";
 }
-
-float Composto(float principal, float taxa, float anos)
+void Composto(float principal, float taxa, float anos)
 { 
   taxa = taxa / 100; //transformando pra usar nas contas
 
@@ -24,6 +44,4 @@ float Composto(float principal, float taxa, float anos)
     cout << "O total de juros a ser pago é: " << juros << "\n";
     cout << "O montante a ser pago é: " << montante << "\n\n";
 }
-
 #endif
-
--- main.cpp
+++ main.cpp
@@ -7,55 +7,54 @@ int main() {
 char menu, opmenu;
 int tela;
 
-do{
-tela=0;
-system("clear");
-cout<<"--------------------------------------\n\n";
-cout<<"***CALCULADORA DE JUROS V1.0***\n\n";
-cout<<"--------------------------------------\n";
-cout<<" 1 - CALCULAR JUROS SIMPLES\n";
-cout<<" 2 - CALCULAR JUROS COMPOSTO\n";
-cout<<" 0 - SAIR\n";
-cout<<"\n \n";
-cout<<"DIGITE UMA OPÇÃO DESEJADA\n";
-menu=getchar();
-switch (menu) {
-  case '1': {
-    Simples();
-    opmenu=getchar();
+  do{
+    tela=0;
+    system("clear");
+    fflush(stdin);
+    cout<<"--------------------------------------\n\n";
+    cout<<"*CALCULADORA DE JUROS V1.0*\n\n";
+    cout<<"--------------------------------------\n";
+    cout<<" 1 - CALCULAR JUROS SIMPLES\n";
+    cout<<" 2 - CALCULAR JUROS COMPOSTO\n";
+    cout<<" 0 - SAIR\n";
+    cout<<"\n \n";
+    cout<<"DIGITE UMA OPÇÃO DESEJADA: ";
+    cin >> menu;
 
     float vInicial=0, taxa=0, tempo=0;
-  
-  cout << "Digite o capital inicial: R$"; cin >> vInicial;
-  cout << "Digite a taxa anual: "; cin >> taxa;
-  cout << "Digite o tempo de aplicacao em anos: "; cin >> tempo;
 
- cout << Simples(vInicial, taxa, tempo);
-    break;
-  }
-  case '2': {
-    Composto();
-    opmenu=getchar();
-    float vInicial=0, taxa=0, tempo=0;
-  
-  cout << "Digite o capital inicial: R$"; cin >> vInicial;
-  cout << "Digite a taxa anual: "; cin >> taxa;
-  cout << "Digite o tempo de aplicacao em anos: "; cin >> tempo;
+    tInvestimentos valores;
+    vInicial = valores.cinValorInicial();
+    taxa = valores.cinTaxa();
+    tempo = valores.cinTempo();
 
- cout << Composto(vInicial, taxa, tempo);
-    break;
-  }
-  case '3': {
-    cout<<"Encerrando...\n\n";
-    tela=1;
- }
+    
+    switch (menu) {
+      case '1': {
+      
+        Simples(vInicial, taxa, tempo);
+        
+        break;
+      }
+      case '2': {
+      
+        Composto(vInicial, taxa, tempo);
+        
+        break;
+      }
+      case '3': {
+        cout<<"Encerrando...\n\n";
+        tela=1;
+        break;
+      }
 
-    break;
-   default: {
-     tela=0;
-     cout<<"OPCAO INVALIDA \n";
-    break;
-   }
-  }
- } while (tela==0);
+      default: {
+        tela=0;
+        cout<<"OPCAO INVALIDA \n";
+        break;  
+      }
+    }
+    char pause=0;
+    cout<<"Digite qualquer coisa para continuar."; cin >> pause;
+  }while (tela==0);
 }
